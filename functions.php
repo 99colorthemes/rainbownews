a644788a06be3d189dc0b1cfc80681e548b04c00
sanyon2015 <lamasakura12@gmail.com>
2016-08-02 22:13:52 +0545

    css change and social icon

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ function rainbownews_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'rainbownews' ),
+		'social' => esc_html__( 'Social Icon', 'rainbownews' )
 	) );
 
 	/*
