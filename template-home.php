b3a8a84194b3d19c47ac33a05a988aa1ab1d5945
sanyon2015 <lamasakura12@gmail.com>
2016-08-05 08:09:54 +0545

    css change

--- template-home.php
+++ template-home.php
@@ -9,107 +9,96 @@
 <?php get_header(); ?>
 
     <!-- trending-start -->
-    <div class="nnc-trending-news">
-        <div class="nnc-container">
+    <div class="nnc-trending-news nnc-clearblock"> 
+        <?php
+        rainbownews_trending_news();
+        ?>
 
-            <?php
-            rainbownews_trending_news();
-            ?>
-
-            <div class="nnc-search nnc-clearblock">
-                <?php get_search_form(); ?>
-            </div>
-        </div>
+        <div class="nnc-search nnc-clearblock">
+            <?php get_search_form(); ?>
+        </div> 
     </div>
     <!-- trending-end -->
 
     <!-- banner-start -->
-    <div class="nnc-highlight-banner">
-        <div class="nnc-container">
-            <div class="nnc-highlight-slider">
-                <?php
-                if (is_active_sidebar('rainbownews_front_page_left_area')) {
-                    if (!dynamic_sidebar('rainbownews_front_page_left_area')):
-                    endif;
-                }
-                ?>
-            </div>
-
-            <div class="nnc-highlight-post">
-                <?php
-                if (is_active_sidebar('rainbownews_front_page_right_area')) {
-                    if (!dynamic_sidebar('rainbownews_front_page_right_area')):
-                    endif;
-                }
-                ?>
-            </div>
-        </div>
-    </div>
-    <!-- banner-end -->
-
-    <!-- latest-start -->
-    <div class="nnc-top-latest">
-        <div class="nnc-container">
+    <div class="nnc-highlight-banner nnc-clearblock"> 
+        <div class="nnc-highlight-slider">
             <?php
-            if (is_active_sidebar('rainbownews_front_page_latest_post_area')) {
-                if (!dynamic_sidebar('rainbownews_front_page_latest_post_area')):
+            if (is_active_sidebar('rainbownews_front_page_left_area')) {
+                if (!dynamic_sidebar('rainbownews_front_page_left_area')):
                 endif;
             }
             ?>
         </div>
+
+        <div class="nnc-highlight-post">
+            <?php
+            if (is_active_sidebar('rainbownews_front_page_right_area')) {
+                if (!dynamic_sidebar('rainbownews_front_page_right_area')):
+                endif;
+            }
+            ?>
+        </div> 
+    </div>
+    <!-- banner-end -->
+
+    <!-- latest-start -->
+    <div class="nnc-top-latest nnc-clearblock"> 
+        <?php
+        if (is_active_sidebar('rainbownews_front_page_latest_post_area')) {
+            if (!dynamic_sidebar('rainbownews_front_page_latest_post_area')):
+            endif;
+        }
+        ?> 
     </div>
     <!-- latest-end -->
 
-    <div id="content" class="content nnc-clearblock">
-        <div class="nnc-container">
-            <div id="primary">
-                <main id="main" class="site-main">
+    <div id="content" class="content nnc-clearblock"> 
+        <div id="primary">
+            <main id="main" class="site-main">
 
+                <?php
+                if (is_active_sidebar('rainbownews_front_page_content_area')) {
+                    if (!dynamic_sidebar('rainbownews_front_page_content_area')):
+                    endif;
+                }
+                ?>
+                <div class="nnc-middle-ads">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/wide-ads2.png" alt="advertisement">
+                </div>
+
+
+                <div class="nnc-middle-ads">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/wide-ads3.png" alt="advertisement">
+                </div>
+
+                <div class="nnc-category nnc-category-layout-3 nnc-left">
                     <?php
-                    if (is_active_sidebar('rainbownews_front_page_content_area')) {
-                        if (!dynamic_sidebar('rainbownews_front_page_content_area')):
+                    if (is_active_sidebar('rainbownews_front_page_middle_left_content_area')) {
+                        if (!dynamic_sidebar('rainbownews_front_page_middle_left_content_area')):
                         endif;
                     }
                     ?>
-                    <div class="nnc-middle-ads">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/wide-ads2.png" alt="advertisement">
-                    </div>
-
-
-                    <div class="nnc-middle-ads">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/wide-ads3.png" alt="advertisement">
-                    </div>
-
-                    <div class="nnc-category nnc-category-layout-3 nnc-left">
-                        <?php
-                        if (is_active_sidebar('rainbownews_front_page_middle_left_content_area')) {
-                            if (!dynamic_sidebar('rainbownews_front_page_middle_left_content_area')):
-                            endif;
-                        }
-                        ?>
-                    </div>
-                    <div class="nnc-category nnc-category-layout-3 nnc-right">
-                        <?php
-                        if (is_active_sidebar('rainbownews_front_page_middle_right_content_area')) {
-                            if (!dynamic_sidebar('rainbownews_front_page_middle_right_content_area')):
-                            endif;
-                        }
-                        ?>
-                    </div>
-
+                </div>
+                <div class="nnc-category nnc-category-layout-3 nnc-right">
                     <?php
-                    if (is_active_sidebar('rainbownews_front_page_bottom_content_area')) {
-                        if (!dynamic_sidebar('rainbownews_front_page_bottom_content_area')):
+                    if (is_active_sidebar('rainbownews_front_page_middle_right_content_area')) {
+                        if (!dynamic_sidebar('rainbownews_front_page_middle_right_content_area')):
                         endif;
                     }
                     ?>
+                </div>
 
-                </main>
-            </div>
-            <?php get_sidebar(); ?>
+                <?php
+                if (is_active_sidebar('rainbownews_front_page_bottom_content_area')) {
+                    if (!dynamic_sidebar('rainbownews_front_page_bottom_content_area')):
+                    endif;
+                }
+                ?>
+
+            </main>
         </div>
-
-
+        <?php get_sidebar(); ?>  
     </div>
 
 <?php get_footer(); ?>
